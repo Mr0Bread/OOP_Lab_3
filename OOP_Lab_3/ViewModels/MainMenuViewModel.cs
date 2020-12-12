7aeafb283d0f6bfd1f91594aa1f369fe3370bf27
Vladislavs Zimnikovs <zimnikovvladislav@gmail.com>
2020-12-12 12:23:30 +0200

    Main Menu Navigation - Fixes in implementation

--- OOP_Lab_3/ViewModels/MainMenuViewModel.cs
+++ OOP_Lab_3/ViewModels/MainMenuViewModel.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using OOP_Lab_3.Base;
 using OOP_Lab_3.Constants;
+using OOP_Lab_3.Events;
 
 namespace OOP_Lab_3.ViewModels
 {
     public class MainMenuViewModel : BindableBase
     {
+        private BindableBase _currentMainMenuViewModel;
+
+        public BindableBase CurrentMainMenuViewModel
+        {
+            get => _currentMainMenuViewModel;
+            set
+            {
+                _currentMainMenuViewModel = value;
+                OnPropertyChanged(nameof(CurrentMainMenuViewModel));
+            }
+        }
+
         public MyICommand<string> NavCommand { get; private set; }
 
         public MainMenuViewModel()
@@ -15,29 +28,7 @@ namespace OOP_Lab_3.ViewModels
 
         private void OnNavigation(string destination)
         {
-            switch (destination)
-            {
-                case NavDestination.Order:
-                    break;
-                case NavDestination.Orders:
-                    break;
-                case NavDestination.Product:
-                    break;
-                case NavDestination.Products:
-                    break;
-                case NavDestination.Customer:
-                    break;
-                case NavDestination.Customers:
-                    break;
-                case NavDestination.User:
-                    break;
-                case NavDestination.Users:
-                    break;
-                case NavDestination.Shipment:
-                    break;
-                case NavDestination.Shipments:
-                    break;
-            }
+           NavigationEvent.GoTo(destination);
         }
     }
 }
